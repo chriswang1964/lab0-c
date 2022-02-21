7a59b3c7a4945de367856cc3a895f64fa21caf03
Chris Wang <jenyu.wang4@gmail.com>
2022-02-21 20:58:49 +0800

    Fill out function q_size

--- queue.c
+++ queue.c
@@ -91,6 +91,14 @@ void q_release_element(element_t *e)
  */
 int q_size(struct list_head *head)
 {
+    if (!head)
+        return 0;
+
+    int len = 0;
+    struct list_head *li;
+
+    list_for_each (li, head)
+        len++;
     return -1;
 }
 
